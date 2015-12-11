1195067748694723a7cb0be0df8c0d552c686d06
Mike Roncone <mjroncone@gmail.com>
2015-12-10 19:18:42 -0600

    feat: calc position function for info box

--- walkthroughplugin.js
+++ walkthroughplugin.js
@@ -1,17 +1,17 @@
 function walkthrough(elements){
-  var ToolTip = {
+  var InfoBox = {
     name : "",
     color : "black",
     bgcolor : "white",
     text : "",
   };
 
-  var tooltipArray = [];
+  var infoBoxArray = [];
 
   elements.forEach(function(element) {
-    ToolTip.name = element.id;
-    ToolTip.text = element.text;
-    tooltipArray.push(ToolTip);
+    InfoBox.name = element.id;
+    InfoBox.text = element.text;
+    infoBoxArray.push(InfoBox);
   });
 
   function initHighlight(element) {
@@ -22,8 +22,47 @@ function walkthrough(elements){
     focus.style.zIndex = 9999;
     focus.style.boxShadow = '0 0 0 9999px rgba(0, 0, 0, 0.5)';
 
-    initToolTip(element, focus);
+    initInfoBox(element, focus);
   }
 
-  initHighlight(tooltipArray.shift());
+  function initInfoBox(element, focus) {
+    var positionLeft = focus.offsetLeft;
+    var positionTop = focus.offsetLeft;
+    var elementWidth = focus.offsetWidth;
+    var elementHeight = focus.offsetHeight;
+    var windowHorizontalMid = window.innerWidth / 2;
+    var windowVerticalMid = window.innerHeight / 2;
+
+    function calcPosition(originX, originY, width, height) {
+      var positionObj = {};
+      var originLeftQuads = originX < windowHorizontalMid ? true : false;
+      var originTopQuads = originY < windowVerticalMid ? true : false;
+      var exclusiveLeftQuad =  originX + width < windowVerticalMid ? true : false;
+
+      if (originLeftQuads && exclusiveLeftQuad) {
+        positionObj.x = originX + width;
+        positionObj.y = originY + height/2;
+        positionObj.fromDirection = "right";
+      } else if (!originLeftQuads) {
+        positionObj.x = originX;
+        positionObj.y = originY + height/2;
+        positionObj.fromDirection = "left";
+      } else if (originTopQuads) {
+        positionObj.x = originX + width/2;
+        positionObj.y = originY + height;
+        positionObj.fromDirection = "bottom";
+      } else if (!originTopQuads) {
+        positionObj.x = originX + width/2;
+        positionObj.y = originY;
+        positionObj.fromDirection = "top";
+      }
+
+    return positionObj;
+
+    }
+
+
+  }
+
+  initHighlight(infoBoxArray.shift());
 }
